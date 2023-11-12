fb9a955d979fc04ec2559a87ff6a62db02226979
omar osama <88536069+OmarSykoo@users.noreply.github.com>
2023-11-12 23:12:01 +0200

    Update Memory.h

--- Memory.h
+++ Memory.h
@@ -1,18 +1,14 @@
-//
-// Created by Sykoo on 11/10/2023.
-//
-
 #ifndef VOL_MACHINE_MEMORY_H
 #define VOL_MACHINE_MEMORY_H
 
 class Memory {
 private:
-    bool in_use {} ;
     int value {} ;
 public:
-    Memory() : in_use(false) , value(0) {}
+    Memory() : value(0) {}
     int get_value();
     void set_value( int val );
+    void clear() ;
 };
 
 #endif //VOL_MACHINE_MEMORY_H
