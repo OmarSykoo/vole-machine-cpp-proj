8d5c305ad5aa42b3af4adefc8ea397f997c7890d
omar osama <88536069+OmarSykoo@users.noreply.github.com>
2023-11-12 23:11:34 +0200

    Update Memory.cpp

--- Memory.cpp
+++ Memory.cpp
@@ -1,16 +1,14 @@
-//
-// Created by Sykoo on 11/10/2023.
-//
-
 #include "Memory.h"
 
 void Memory::set_value( int val ){
-    in_use = true ;
     value = val ;
 }
 
 int Memory::get_value() {
-    if ( in_use )
-        return value ;
-    throw std::invalid_argument("Garbage!M\n") ;
+    return value;
 }
+
+void Memory::clear() {
+    value = false ;
+}
+
