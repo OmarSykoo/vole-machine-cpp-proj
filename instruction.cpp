e495293cc90f69288abda802018f2d2dd09f482f
omar osama <88536069+OmarSykoo@users.noreply.github.com>
2023-11-12 20:56:04 +0200

    Update instruction.cpp

--- instruction.cpp
+++ instruction.cpp
@@ -4,49 +4,44 @@
 
 #include "instruction.h"
 
-int instruction::process( Register *R , Memory *M , int l , int r , int step ){
-    throw std::invalid_argument("this function is meant to be overridden\n") ;
-    return step + 2 ;
-}
-
-int _RXY1::process(Register *R, Memory *M, int l, int r , int step ){
+int _RXY1::process( Register *R , Memory *M, int l, int r , int step ){
     R[l].set_value(M[r].get_value()) ;
     return step + 2 ;
 }
 
-int _RXY2::process(Register *R, Memory *M, int l, int r , int step ) {
+int _RXY2::process( Register *R , Memory *M, int l, int r , int step ) {
     R[l].set_value(r) ;
     return step + 2 ;
 }
 
-int _RXY3::process(Register *R, Memory *M, int l, int r , int step ) {
+int _RXY3::process( Register *R , Memory *M, int l, int r , int step ) {
     if ( r == 0 ) {
-        std::cout << R[l].get_value() << '\n' ;
-        return step + 2 ;
+        std::cout << R[l].get_value() << ' ' ;
+        std::cout << (char)R[l].get_value() << '\n' ;
     }
     M[r].set_value(R[l].get_value()) ;
     return step + 2 ;
 }
 
-int _0RS::process(Register *R, Memory *M, int l, int r , int step) {
+int _0RS::process( Register *R , Memory *M, int l, int r , int step) {
     int left_bit = r & 0xF0 , right_bit = r & 0x0F ;
     R[right_bit].set_value(R[ left_bit >> 4 ].get_value() ) ;
     return step + 2 ;
 }
 
-int _RST::process(Register *R, Memory *M, int l, int r , int step ) {
+int _RST::process( Register *R , Memory *M, int l, int r , int step ) {
     int left_bit = r & 0xF0 , right_bit = r & 0x0F ;
-    R[l].set_value( R[(left_bit >> 4)].get_value() + R[right_bit].get_value() ) ;
+    R[l].set_value( (R[(left_bit >> 4)].get_value() + R[right_bit].get_value()) & 0xFF ) ;
     return step + 2 ;
 }
 
-int _RXYB::process(Register *R, Memory *M, int l, int r , int step ) {
+int _RXYB::process( Register *R , Memory *M, int l, int r , int step ) {
     if ( R[0].get_value() == R[l].get_value() ) {
         return r ;
     }
     return step + 2 ;
 }
 
-int _000::process(Register *R, Memory *M, int l, int r, int step) {
+int _000::process( Register *R , Memory *M, int l, int r, int step) {
     return -1 ;
 }
