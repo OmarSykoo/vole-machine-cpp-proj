fa7a208a83feafadfb25a96a07fcf1e9477fbfb3
omar osama <88536069+OmarSykoo@users.noreply.github.com>
2023-11-12 20:58:32 +0200

    Update Register.h

--- Register.h
+++ Register.h
@@ -5,15 +5,15 @@
 #ifndef VOL_MACHINE_REGISTER_H
 #define VOL_MACHINE_REGISTER_H
 
-
 class Register {
 private:
     bool in_use {} ;
     int value {} ;
 public:
     Register() : in_use( false ) , value( 0 ) {}
-    int get_value();
-    void set_value( int val );
+    int get_value() const;
+    void set_value( int val ) ;
 };
 
 #endif //VOL_MACHINE_REGISTER_H
+
