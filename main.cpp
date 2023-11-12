3f0ccac1a79eddd28be09053dfa782dbe895fbfb
omar osama <88536069+OmarSykoo@users.noreply.github.com>
2023-11-12 23:35:48 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -23,77 +23,46 @@ private :
 public:
     Machine()  ;
     ~Machine() ;
-    friend class Fetch_info ;
+    void run(){
+        while ( step != -1 && ( step / 2 < data_.size() ) ) {
+            pair<int,int> temp = data_[ step / 2] ;
+            step = instructions[ ( ( (temp.first & 0xF0)) >> 4 ) ]->process(registers , addresses, (temp.first & 0x0F) , temp.second , step );
+        }
+    }
+    void single_step(){
+        pair<int,int> temp = data_[step / 2] ;
+        step = instructions[ ( ( (temp.first & 0xF0)) >> 4 ) ]->process( registers , addresses, (temp.first & 0x0F) , temp.second , step );
+    }
     friend class Run_mach ;
-    friend class Clear ;
-    friend class print_info ;
-};
-
-class print_info {
-private:
-    Machine *a ;
-public:
-    print_info( Machine &pass ) : a(&pass) {}
+    void fetch( const string& text ){
+        ifstream fin(text + ".txt" ) ;
+        if ( fin.fail() ){
+            throw invalid_argument("can't open file \n") ;
+        }
+        int R , XY ;
+        while ( fin >> hex >> R >> XY ) {
+            data_.emplace_back( R , XY ) ;
+        }
+    }
+    void clear_memory() {
+        for ( auto& i : registers){
+            i.clear() ;
+        }
+        for ( auto& i : addresses ) {
+            i.clear() ;
+        }
+        step = 0 ;
+    }
     void print(){
         cout << "N R 0 1 2 3 4 5 6 7 8 9 A B C D E F\n" ;
         for ( int i = 0 ; i < 16 ; i++ ){
             cout << hex << i << ' ' ;
-            cout << hex << a->registers[i].get_value() << ' ' ;
+            cout << hex << registers[i].get_value() << ' ' ;
             for ( int j = 0 ; j < 16 ; j++ ){
-                cout << hex << a->addresses[ ( j << 4 ) + i ].get_value() << ' ' ;
+                cout << hex << addresses[ ( j << 4 ) + i ].get_value() << ' ' ;
             }
             cout << '\n' ;
         }
-    }
-};
-
-class Clear {
-private:
-    Machine *a ;
-public:
-    Clear( Machine &pass ) : a(&pass) {}
-    void clear_memory() {
-        for ( auto& i : a->registers){
-            i.clear() ;
-        }
-        for ( auto& i : a->addresses ) {
-            i.clear() ;
-        }
-        a->step = 0 ;
-    }
-};
-
-class Fetch_info {
-private:
-    Machine *a ;
-public:
-    Fetch_info( Machine &pass ) : a(&pass) {}
-    void fetch( const string& text ){
-        ifstream fin(text + ".txt" ) ;
-        if ( fin.fail() ){
-            throw invalid_argument("can't opent file \n") ;
-        }
-        int R , XY ;
-        while ( fin >> hex >> R >> XY ) {
-            a->data_.emplace_back( R , XY ) ;
-        }
-    }
-};
-
-class Run_mach {
-private:
-    Machine *a ;
-public:
-    Run_mach( Machine &pass ) : a( &pass ) {}
-    void run(){
-        while ( a->step != -1 && ( a->step / 2 < a->data_.size() ) ) {
-            pair<int,int> temp = a->data_[a->step / 2] ;
-            a->step = a->instructions[ ( ( (temp.first & 0xF0)) >> 4 ) ]->process(a->registers , a->addresses, (temp.first & 0x0F) , temp.second , a->step );
-        }
-    }
-    void single_step(){
-        pair<int,int> temp = a->data_[a->step / 2] ;
-        a->step = a->instructions[ ( ( (temp.first & 0xF0)) >> 4 ) ]->process(a->registers , a->addresses, (temp.first & 0x0F) , temp.second , a->step );
     }
 };
 
@@ -115,10 +84,6 @@ Machine::~Machine() {
 
 int main(){
     Machine vol ;
-    Run_mach Run_machine( vol ) ;
-    print_info print_memory(vol) ;
-    Fetch_info Fetch_(vol) ;
-    Clear clear_memory( vol ) ;
     bool logged_in = true ;
     while ( logged_in ){
         cout << "1) Load Data \n" ;
@@ -133,23 +98,23 @@ int main(){
                 logged_in = false ;
                 break;
             case '1' :
-                Fetch_.fetch("input") ;
+                vol.fetch("input") ;
                 cout << "Data_loaded\n" ;
                 break;
             case '2' :
-                clear_memory.clear_memory() ;
+                vol.clear_memory() ;
                 cout << "Cleared memory\n" ;
                 break;
             case '3' :
-                Run_machine.run() ;
+                vol.run() ;
                 cout << "Running..\n" ;
                 break;
             case '4' :
-                Run_machine.single_step() ;
+                vol.single_step() ;
                 cout << "Next_step...\n" ;
                 break;
             case '5' :
-                print_memory.print() ;
+                vol.print() ;
                 cout << "printing_info...\n" ;
                 break;
             default:
