5b02a3268764f11e8f8ff21fd37b4c154bf415ea
omar osama <88536069+OmarSykoo@users.noreply.github.com>
2023-11-12 20:59:20 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <map>
 #include <string>
 #include <fstream>
 #include "Memory.h"
@@ -10,7 +11,6 @@
 #include "instruction.cpp"
 using namespace std;
 
-
 vector<instruction> all_instructions ;
 
 class Machine {
@@ -18,54 +18,60 @@ private :
     Register registers[16] ;
     Memory addresses[256] ;
     vector<pair<int,int>> data_ ;
-    vector<instruction*> instructions ;
+    map< int , instruction* > instructions ;
     int step {0} ;
 public:
     Machine() ;
     ~Machine() ;
-    void fetch( const string& text ) ;
     void run() ;
+    friend class Fetch_info ;
 };
 
-void Machine::fetch( const string& text ) {
-    ifstream fin(text + ".txt" ) ;
-    if ( fin.fail() ){
-        throw invalid_argument("can't opent file \n") ;
+class Fetch_info {
+private:
+    Machine *a ;
+public:
+    Fetch_info( Machine &pass ) : a(&pass) {}
+    void fetch( const string& text ){
+        ifstream fin(text + ".txt" ) ;
+        if ( fin.fail() ){
+            throw invalid_argument("can't opent file \n") ;
+        }
+        int R , XY ;
+        while ( fin >> hex >> R >> XY ) {
+            a->data_.emplace_back( R , XY ) ;
+        }
     }
-    int R , XY ;
-    while ( fin >> hex >> R >> XY ) {
-        data_.emplace_back( R , XY ) ;
-    }
-}
+};
 
 void Machine::run() {
     while ( step != -1 && ( step / 2 < data_.size() ) ) {
-        pair<int,int> temp = data_[step / 2];
-        step = instructions[ ( ( (temp.first & 0xF0)) >> 4 ) - 1 ]->process(registers , addresses, (temp.first & 0x0F) , temp.second , step );
+        pair<int,int> temp = data_[step / 2] ;
+        step = instructions[ ( ( (temp.first & 0xF0)) >> 4 ) ]->process(registers , addresses, (temp.first & 0x0F) , temp.second , step );
     }
 }
 
 Machine::Machine() {
-    instructions.resize( 8) ;
-    instructions[0] = new _RXY1 ;
-    instructions[1] = new _RXY2 ;
-    instructions[2] = new _RXY3 ;
-    instructions[3] = new _0RS  ;
-    instructions[4] = new _RST  ;
-    instructions[5] = new _RXY1 ;
-    instructions[6] = new _RXYB ;
-    instructions[7] = new _000  ;
+    instructions[0x1] = new _RXY1 ;
+    instructions[0x2] = new _RXY2 ;
+    instructions[0x3] = new _RXY3 ;
+    instructions[0x4] = new _0RS  ;
+    instructions[0x5] = new _RST  ;
+    instructions[0xB] = new _RXYB ;
+    instructions[0xC] = new _000  ;
 }
 
 Machine::~Machine() {
-    for ( int i = 0 ; i < 8 ; i++ )
-        delete instructions[i] ;
+    for ( auto &i : instructions )
+        delete i.second ;
     instructions.clear() ;
 }
 
 int main(){
     Machine vol ;
+    Fetch_info F(vol) ;
     string text ; cin >> text ;
-    vol.fetch(text) ;
+    F.fetch( text ) ;
     vol.run() ;
+    return 0 ;
 }
